e5b383b42f11522252b189b1c3f616881dbbd929
山谷悠喜 <yamayayuuki@yamataniyuukinoMacBook-Air.local>
2026-06-07 21:00:43 +0900

    初回コミット

---
+++ script.js
@@ -0,0 +1,38 @@
+// フィルタ表示
+const buttons = document.querySelectorAll("button");
+const list = document.querySelectorAll("li");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const filter = button.dataset.id;
+
+        list.forEach((li => {
+            const id = li.dataset.id;
+
+            if (filter === id) {
+                li.style.display = "";
+            } else {
+                li.style.display = "none";
+            }
+        }));
+    });
+});
+
+// ボタンをクリックしたら背景色を赤くする
+const btns = document.querySelectorAll("button");
+
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+
+        // 全部リセット
+        buttons.forEach(b => {
+            b.style.backgroundColor = "";
+            b.style.color = "";
+        });
+
+        // クリックしたやつだけ適用
+        btn.style.backgroundColor = "#ED1A3D";
+        btn.style.color = "#fff";
+    });
+});
